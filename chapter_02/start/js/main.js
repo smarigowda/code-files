dfb4e53338005765738f146ba8796f353e18e561
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-01-11 20:11:44 +0000

    setting DOM attributes dynamically

--- chapter_02/start/js/main.js
+++ chapter_02/start/js/main.js
@@ -4,14 +4,14 @@ const svg = d3
   .attr("viewBox", "0 0 1200 1600")
   .style("border", "1px solid black");
 
-svg
-  .append("rect")
-  .attr("x", 10)
-  .attr("y", 10)
-  .attr("width", 414)
-  .attr("height", 16)
-  .attr("fill", "turquoise")
-  .style("fill", "plum");
+// svg
+//   .append("rect")
+//   .attr("x", 10)
+//   .attr("y", 10)
+//   .attr("width", 414)
+//   .attr("height", 16)
+//   .attr("fill", "turquoise")
+//   .style("fill", "plum");
 
 const dataParsed = d3.csv("./data/data.csv", (data) => {
   // console.log(data);
@@ -37,7 +37,24 @@ dataParsed.then((data) => {
   createViz(data);
 });
 
+const barHeight = 20;
+
 const createViz = (data) => {
-  console.log('--- createViz ---')
+  console.log("--- createViz ---");
   console.log(data);
+  svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("class", (d) => {
+      console.log(d);
+      return `bar bar-${d.technology}`;
+    })
+    .attr("width", (d) => d.count)
+    .attr("height", barHeight)
+    .attr("x", 0)
+    .attr("y", (d, i) => (barHeight + 5) * i)
+    .attr("fill", (d) =>
+      d.technology === "D3.js" ? "yellowgreen" : "skyblue"
+    );
 };
