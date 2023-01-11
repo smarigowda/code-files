aad7a803e55b6bc64889f200e61c58dee7bd36c5
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-01-11 05:48:01 +0000

    load and measure data

--- chapter_02/start/js/main.js
+++ chapter_02/start/js/main.js
@@ -2,7 +2,7 @@ const svg = d3
   .select(".responsive-svg-container")
   .append("svg")
   .attr("viewBox", "0 0 1200 1600")
-  .style('border', '1px solid black')
+  .style("border", "1px solid black");
 
 svg
   .append("rect")
@@ -10,5 +10,34 @@ svg
   .attr("y", 10)
   .attr("width", 414)
   .attr("height", 16)
-  .attr('fill', 'turquoise')
-  .style('fill', 'plum')
+  .attr("fill", "turquoise")
+  .style("fill", "plum");
+
+const dataParsed = d3.csv("./data/data.csv", (data) => {
+  // console.log(data);
+  return {
+    technology: data.technology,
+    count: +data.count,
+  };
+});
+
+dataParsed.then((data) => {
+  data.sort((a, b) => b.count - a.count);
+  console.log(data);
+  console.log(data.columns);
+  data.forEach((d) => {
+    console.log(d);
+  });
+  console.log(data.length);
+  console.log(d3.max(data, (d) => d.count));
+  console.log(d3.min(data, (d) => d.count));
+
+  console.log(d3.extent(data, (d) => d.count));
+
+  createViz(data);
+});
+
+const createViz = (data) => {
+  console.log('--- createViz ---')
+  console.log(data);
+};
