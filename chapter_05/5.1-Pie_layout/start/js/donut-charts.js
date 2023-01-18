1ea97727ab0ba06bf2d23ed0d8b7adefbeee8d0f
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-01-18 19:31:55 +0000

    prep complete

--- chapter_05/5.1-Pie_layout/start/js/donut-charts.js
+++ chapter_05/5.1-Pie_layout/start/js/donut-charts.js
@@ -1,5 +1,20 @@
 const drawDonutCharts = (data) => {
   // Generate the donut charts here
 
-  
+  const svg = d3
+    .select("#donut")
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`);
+
+  const donutContainers = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  const years = [1975, 1995, 2013];
+
+  years.forEach((year) => {
+    donutContainers
+      .append("g")
+      .attr("transform", `translate(${xScale(year)}, ${innerHeight / 2})`);
+  });
 };
