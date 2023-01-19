019b58c9dfe715244e40a877eae1291f1516e865
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-01-19 05:27:33 +0000

    data formatted

--- chapter_05/5.1-Pie_layout/start/js/donut-charts.js
+++ chapter_05/5.1-Pie_layout/start/js/donut-charts.js
@@ -11,10 +11,20 @@ const drawDonutCharts = (data) => {
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   const years = [1975, 1995, 2013];
+  const musicFormats = data.columns.filter(format => format !== 'year')
 
   years.forEach((year) => {
+    const yearData = data.find(d => d.year === year)
+    console.log(yearData)
+    const formattedData = []
+    musicFormats.forEach( format => {
+      formattedData.push({ format, sales:  yearData[format]})
+    })
+    console.log(formattedData)
     donutContainers
       .append("g")
       .attr("transform", `translate(${xScale(year)}, ${innerHeight / 2})`);
   });
+
+
 };
