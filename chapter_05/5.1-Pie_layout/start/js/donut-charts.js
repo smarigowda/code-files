795a88c1e2335b377f5715aacbacfefe546d447d
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-01-20 16:59:15 +0000

    geneerated arcs

--- chapter_05/5.1-Pie_layout/start/js/donut-charts.js
+++ chapter_05/5.1-Pie_layout/start/js/donut-charts.js
@@ -11,20 +11,38 @@ const drawDonutCharts = (data) => {
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   const years = [1975, 1995, 2013];
-  const musicFormats = data.columns.filter(format => format !== 'year')
+  const musicFormats = data.columns.filter((format) => format !== "year");
 
   years.forEach((year) => {
-    const yearData = data.find(d => d.year === year)
-    console.log(yearData)
-    const formattedData = []
-    musicFormats.forEach( format => {
-      formattedData.push({ format, sales:  yearData[format]})
-    })
-    console.log(formattedData)
-    donutContainers
+    const yearData = data.find((d) => d.year === year);
+    // console.log(yearData)
+    const formattedData = [];
+    musicFormats.forEach((format) => {
+      formattedData.push({ format, sales: yearData[format] });
+    });
+    // console.log(formattedData)
+    const donutContainer = donutContainers
       .append("g")
       .attr("transform", `translate(${xScale(year)}, ${innerHeight / 2})`);
+
+    const pieGenerator = d3.pie().value((d) => d.sales);
+    const annotatedDate = pieGenerator(formattedData);
+    console.log(annotatedDate);
+
+    const arcGenerator = d3
+      .arc()
+      .startAngle((d) => d.startAngle)
+      .endAngle((d) => d.endAngle)
+      .innerRadius(60)
+      .outerRadius(100)
+      .padAngle(0.02)
+      .cornerRadius(3);
+
+    const arcs = donutContainer
+      .selectAll(`path.arc-${year}`)
+      .data(annotatedDate)
+      .join("path")
+      .attr("class", `arc-${year}`)
+      .attr("d", arcGenerator);
   });
-
-
 };
