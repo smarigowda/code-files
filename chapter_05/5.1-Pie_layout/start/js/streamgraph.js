ba86be83a08d7106a32b0923d1d0d49d38070f48
Santosh Marigowda <santosh.marigowda@abcam.com>
2023-01-21 19:47:15 +0000

    first iteration of stream graph complete

--- chapter_05/5.1-Pie_layout/start/js/streamgraph.js
+++ chapter_05/5.1-Pie_layout/start/js/streamgraph.js
@@ -10,6 +10,21 @@ const drawStreamGraph = (data) => {
     .append("svg")
     .attr("viewBox", [0, 0, width, height]);
 
+  const leftAxisLabel = svg.append("text").attr("dominant-baseline", "hanging");
+
+  leftAxisLabel.append("tspan").text("Total revenue");
+  leftAxisLabel
+    .append("tspan")
+    .text("(million USD)")
+    .attr("dx", 5)
+    .attr("fill-opacity", 0.7);
+  leftAxisLabel
+    .append("tspan")
+    .text("Adjusted for inflation")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("fill-opacity", 0.7).style('font-size', '14px')
+
   const innerChart = svg
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
